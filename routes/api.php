e4b69f447b21bd1d3474f3a2e1ba1d54e076ce1c
ryotarofr <ryoryo.fr0608@icloud.com>
2024-01-07 10:33:24 +0900

    startrt rogic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('student', function () {
-    return 'i am student';
-});
+Route::get('student', [StudentController::class, 'index']);
